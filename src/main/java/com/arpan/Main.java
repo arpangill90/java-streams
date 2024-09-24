d907e919024579b1dd5aa6a80f3e713bdf24029f
arpangill90 <arpangill90@gmail.com>
2024-09-24 07:29:00 -0400

    very basic use of java stream and filter to get even numbers

--- src/main/java/com/arpan/Main.java
+++ src/main/java/com/arpan/Main.java
@@ -6,10 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         printNumbers(List.of(1,2,3,4));
+        printEvenNumbers(List.of(1,2,3,4));
     }
 
     //Simple print using method reference
     private static void printNumbers(List<Integer> numbers){
         numbers.forEach(System.out::println); //for each integer in list, call this method
+    }
+
+    private static void printEvenNumbers(List<Integer> numbers){
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
     }
 }
