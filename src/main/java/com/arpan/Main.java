f701443e72fc754246749f9884fb9ba8bcff11d4
arpangill90 <arpangill90@gmail.com>
2024-09-24 07:22:39 -0400

    initial commit with basic print

---
+++ src/main/java/com/arpan/Main.java
@@ -0,0 +1,15 @@
+package com.arpan;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        printNumbers(List.of(1,2,3,4));
+    }
+
+    //Simple print using method reference
+    private static void printNumbers(List<Integer> numbers){
+        numbers.forEach(System.out::println); //for each integer in string, call this method
+    }
+}
