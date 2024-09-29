41bc6041ac861d67a7824abcc4c605b37eb7f80f
arpangill90 <arpangill90@gmail.com>
2024-09-29 17:24:58 -0400

    combine filter and reduce

--- src/main/java/com/arpan/StreamsExamples.java
+++ src/main/java/com/arpan/StreamsExamples.java
@@ -10,10 +10,17 @@ public class StreamsExamples {
 
     public static void main(String[] args) {
         System.out.println(sumOfList(List.of(1, 2, 3, 4, 5)));
+        System.out.println(sumOfOddNumbers(List.of(1, 2, 3, 4, 5)));
     }
 
     private static int sumOfList(List<Integer> numbers) {
         return numbers.stream()
-                .reduce(0, Integer::sum); //to use lambda .reduced(0, (a,b) -> a+b)
+                .reduce(0, Integer::sum); //to use lambda .reduce(0, (a,b) -> a+b)
+    }
+
+    private static int sumOfOddNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n ->  n%2 != 0)
+                .reduce(0, Integer::sum);
     }
 }
