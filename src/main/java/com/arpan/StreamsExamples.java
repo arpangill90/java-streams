4dc45493c7f2386cab8f459174d1484811a6ff9b
arpangill90 <arpangill90@gmail.com>
2024-09-29 17:45:00 -0400

    Add sort examples

--- src/main/java/com/arpan/StreamsExamples.java
+++ src/main/java/com/arpan/StreamsExamples.java
@@ -1,10 +1,11 @@
 package com.arpan;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Contains examples for
- * Reduce - to do sum of numbers in a list
+ * Reduce
  */
 public class StreamsExamples {
 
@@ -13,6 +14,10 @@ public class StreamsExamples {
         System.out.println(sumOfOddNumbers(List.of(1, 2, 3, 4, 5)));
         System.out.println(sumOfSquareOfEvenNumbers(List.of(1, 2, 3, 4, 5)));
         printDistinctNumbers(List.of(5, 10, 13, 13, 2, 5, 4, 3, 4, 5));
+        sortAndPrintNumbers(List.of(5, 10, 13, 13, 2, 5, 4, 3, 4, 5));
+        sortAndPrintValues(List.of("", "JOE", "MOE", "MARY"));
+        reverseSortAndPrintValues(List.of("", "JOE", "MOE", "MARY"));
+
     }
 
     private static int sumOfList(List<Integer> numbers) {
@@ -35,5 +40,23 @@ public class StreamsExamples {
 
     private static void printDistinctNumbers(List<Integer> numbers) {
         numbers.stream().distinct().forEach(System.out::println);
+    }
+
+    private static void sortAndPrintNumbers(List<Integer> numbers) {
+        numbers.stream().sorted().forEach(System.out::println);
+    }
+
+    private static void sortAndPrintValues(List<String> names) {
+        names.stream()
+                .filter(name -> !name.isEmpty())
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static void reverseSortAndPrintValues(List<String> names) {
+        names.stream()
+                .filter(name -> !name.isEmpty())
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
     }
 }
