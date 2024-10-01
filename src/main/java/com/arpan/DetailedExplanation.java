49da8e6d51029fe75140a0eec718e8fbb7386586
arpangill90 <arpangill90@gmail.com>
2024-09-30 21:02:53 -0400

    code clean up

--- src/main/java/com/arpan/DetailedExplanation.java
+++ src/main/java/com/arpan/DetailedExplanation.java
@@ -7,6 +7,7 @@ import java.util.function.Predicate;
 public class DetailedExplanation {
     public static void main(String[] args) {
         printEvenNumbers(List.of(1, 2, 3, 4));
+        printSquareOfEvenNumbers(List.of(1, 2, 3, 4));
     }
 
     private static void printEvenNumbers(List<Integer> numbers){
