7814e39bf02462f744999d0f1240a285dd043d5f
arpangill90 <arpangill90@gmail.com>
2024-09-30 21:03:59 -0400

    adding code comments

--- src/main/java/com/arpan/DetailedExplanation.java
+++ src/main/java/com/arpan/DetailedExplanation.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * This class has some details of what happens behind the scenes with functional Java
+ */
 public class DetailedExplanation {
     public static void main(String[] args) {
         printEvenNumbers(List.of(1, 2, 3, 4));
