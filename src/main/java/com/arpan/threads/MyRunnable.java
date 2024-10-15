ebaabf23900c9930af36de4f87d08a945fe732b3
arpangill90 <arpangill90@gmail.com>
2024-10-15 09:00:48 -0400

    2 samples of threads added

--- src/main/java/com/arpan/threads/MyRunnable.java
+++ src/main/java/com/arpan/threads/MyRunnable.java
@@ -1,6 +1,9 @@
 package com.arpan.threads;
 
 public class MyRunnable implements Runnable{
+
+
+
     @Override
     public void run() {
         System.out.println("Thread run");
--- src/main/java/com/arpan/threads/ThreadMain.java
+++ src/main/java/com/arpan/threads/ThreadMain.java
@@ -3,10 +3,24 @@ package com.arpan.threads;
 public class ThreadMain {
 
     public static void main(String[] args) {
-        MyRunnable myRunnable1 = new MyRunnable();
-        MyRunnable myRunnable2 = new MyRunnable();
 
-        myRunnable1.run();
-        myRunnable2.run();
+        //The first example - each thread has its own runnable - separate reference in the heap
+        MyRunnable myRunnable1 = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable1, "Thread-1");
+
+        MyRunnable myRunnable2 = new MyRunnable();
+        Thread thread2 = new Thread(myRunnable2, "Thread-2");
+
+        thread1.start();
+        thread2.start();
+
+        //The second example - 2 threads using same runnable - same reference in the heap
+        //thread stack is different
+        MyRunnable myRunnable3 = new MyRunnable();
+        Thread thread3 = new Thread(myRunnable3, "Thread-3");
+        Thread thread4 = new Thread(myRunnable3, "Thread-4");
+
+        thread3.start();
+        thread4.start();
     }
 }
