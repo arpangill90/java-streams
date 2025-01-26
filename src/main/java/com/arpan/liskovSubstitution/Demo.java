c188ee3d2f0cadf1c627ec95f35dbdd6c8675b29
arpangill90 <arpangill90@gmail.com>
2025-01-26 12:10:47 -0500

    update demo

--- src/main/java/com/arpan/liskovSubstitution/Demo.java
+++ src/main/java/com/arpan/liskovSubstitution/Demo.java
@@ -11,11 +11,19 @@ public class Demo {
         items.add(new JacketItem());
 
         int total = getTotalPrice(items);
+        System.out.println("Total of cart one = "+total);
 
-        System.out.println(total);
+        List<ShirtItem> shirts = new ArrayList<>();
+        shirts.add(new ShirtItem());
+        shirts.add(new ShirtItem());
+
+        int totalPrice = getTotalPrice(shirts);//this call works as we have wild card in the method
+        System.out.println("Total price for shirts = " + totalPrice);
+
+
     }
 
-    private static int getTotalPrice(List<ClothingItem> items) {
+    private static int getTotalPrice(List<? extends ClothingItem> items) {
         return items.stream()
                 .map(ClothingItem::price)
                 .reduce(0, Integer::sum);
